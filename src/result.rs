267e66358359117f488ee11dfc0faa810be14846
N <somedevfox@gmail.com>
2023-04-13 18:38:47 +0300

    Modify `result` module APIs
     + Document variants of `Error` enumeration
     + Document `Result<T>` type
     + Implement `std::fmt::Display` and `std::error::Error` trait on `Error` enumeration

--- src/result.rs
+++ src/result.rs
@@ -13,17 +13,46 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with fileext. If not, see <http://www.gnu.org/licenses/>.
 
+use std::error;
+use std::fmt::Display;
 use std::io;
 
+/// Error type for Input/Output operations.
 #[derive(Debug)]
 pub enum Error {
+    /// The executable which the application is trying to get doesn't exist in the filesystem.
     ExecutableDoesntExist,
+    /// Operating System I/O Error
     Io(io::Error),
 }
+
+// Display traits
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Error::{ExecutableDoesntExist, Io};
+
+        if let Io(why) = self {
+            why.fmt(f)
+        } else {
+            write!(
+                f,
+                "{}",
+                match self {
+                    ExecutableDoesntExist => "executable doesn't exist",
+                    _ => unreachable!(),
+                }
+            )
+        }
+    }
+}
+impl error::Error for Error {}
+
+// Constructor traits
 impl From<io::Error> for Error {
     fn from(value: io::Error) -> Self {
         Self::Io(value)
     }
 }
 
+/// A type that represents either success ([Ok](std::result::Result::Ok)) or failure ([Error]).
 pub type Result<T> = core::result::Result<T, Error>;
